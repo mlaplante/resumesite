10a38f856313f859db3bb68b99e881f2956245d1
Michael La Plante <mlaplante@Michaels-MacBook-Pro.local>
2017-01-28 11:34:43 -0500

    creating 2017 site

--- js/script.js
+++ js/script.js
@@ -1,278 +1,188 @@
-/*-----------------------------------------------------------------------------------
-/*
-/* Script for Resume
-/*
------------------------------------------------------------------------------------*/
-
-/*----------------------------------------------------*/
-/* Preloader
------------------------------------------------------- */
-
-$(window).load(function() {
-
-    $('.loader').fadeOut();
-    $('#preloader').delay(350).fadeOut('slow');
-    $('body').delay(350);
-
-});
-
-
-jQuery(document).ready(function($) {
-
-    /*----------------------------------------------------*/
-    /* Initializing jQuery Nice Scroll
-    ------------------------------------------------------ */
-
-    $("html").niceScroll({
-        cursorcolor: "#11abb0", // Set cursor color
-        cursorwidth: "8", // Sety cursor width
-        cursorborder: "" // Set cursor border color, default left none
-    });
-
-
-    /*----------------------------------------------------*/
-    /* FitText Settings
-    ------------------------------------------------------ */
-
-    setTimeout(function() {
-        $('h1.responsive-headline').fitText(1, {
-            minFontSize: '28px',
-            maxFontSize: '72px'
-        });
-    }, 100);
-
-
-    /*----------------------------------------------------*/
-    /* Smooth Scrolling
-    ------------------------------------------------------ */
-
-    $('.smoothscroll').on('click', function(e) {
-        e.preventDefault();
-
-        var target = this.hash,
-            $target = $(target);
-
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 800, 'swing', function() {
-            window.location.hash = target;
-        });
-
-    });
-
-
-    /*----------------------------------------------------*/
-    /* Appear Animation
-    ------------------------------------------------------*/
-    new WOW().init();
-
-    /*----------------------------------------------------*/
-    /* Parallax for Header Content
-    ------------------------------------------------------*/
-    $(window).scroll(function(e) {
-        parallax();
-    });
-
-
-    function parallax() {
-        var scrollPosition = $(window).scrollTop();
-        $('.banner').css('margin-top', (0 - (scrollPosition * .8)) + 'px');
-    }
-
-    /*----------------------------------------------------*/
-    /* Highlight the current section in the navigation bar
-    ------------------------------------------------------*/
-
-    var sections = $("section");
-    var navigation_links = $("#m-nav a");
-
-    sections.waypoint({
-
-        handler: function(event, direction) {
-
-            var active_section;
-
-            active_section = $(this);
-            if (direction === "up") active_section = active_section.prev();
-
-            var active_link = $('#m-nav a[href="#' + active_section.attr("id") + '"]');
-
-            navigation_links.parent().removeClass("current");
-            active_link.parent().addClass("current");
-
-        },
-        offset: '35%'
-
-    });
-
-
-    /*----------------------------------------------------*/
-    /*  Make sure that #header-background-image height is
-    /* equal to the browser height.
-    ------------------------------------------------------ */
-
-    $('header').css({
-        'height': $(window).height()
-    });
-    $(window).on('resize', function() {
-
-        $('header').css({
-            'height': $(window).height()
-        });
-        $('body').css({
-            'width': $(window).width()
-        })
-    });
-
-
-    /*----------------------------------------------------*/
-    /*  On scroll blur header
-    ------------------------------------------------------*/
-    (function() {
-        $(window).scroll(function() {
-            var oVal;
-            oVal = $(window).scrollTop() / 100;
-            return $(".header-overlay").css("opacity", oVal);
-        });
-
-    }).call(this);
-
-
-
-    /*----------------------------------------------------*/
-    /*  Fade In/Out Primary Navigation
-    ------------------------------------------------------*/
-
-    $(window).on('scroll', function() {
-
-        var h = $('header').height();
-        var y = $(window).scrollTop();
-        var nav = $('#m-nav');
-
-        if ((y > h * .20) && (y < h) && ($(window).outerWidth() > 768)) {
-            nav.fadeOut('fast');
-        } else {
-            if (y < h * .20) {
-                nav.removeClass('opaque').fadeIn('fast');
-            } else {
-                nav.addClass('opaque').fadeIn('fast');
-            }
-        }
-
-    });
-
-
-    /*----------------------------------------------------*/
-    /*  Modal Popup
-    ------------------------------------------------------*/
-
-    $('.item-wrap a').magnificPopup({
-
-        type: 'inline',
-        fixedContentPos: false,
-        removalDelay: 200,
-        showCloseBtn: false,
-        mainClass: 'mfp-fade'
-
-    });
-
-    $(document).on('click', '.popup-modal-dismiss', function(e) {
-        e.preventDefault();
-        $.magnificPopup.close();
-    });
-
-
-    /*----------------------------------------------------*/
-    /*  Owl Carousel
-    /*----------------------------------------------------*/
-
-
-    $(document).ready(function() {
-
-        $("#testimonial-slides").owlCarousel({
-
-            navigation: false, // Show next and prev buttons
-            slideSpeed: 300,
-            paginationSpeed: 400,
-            singleItem: true
-
-            // "singleItem:true" is a shortcut for:
-            // items : 1,
-            // itemsDesktop : false,
-            // itemsDesktopSmall : false,
-            // itemsTablet: false,
-            // itemsMobile : false
-
-        });
-
-    });
-
-
-    /*----------------------------------------------------*/
-    /*  Google Map
-    ------------------------------------------------------*/
-
-    // main directions
-    // map = new GMaps({
-    //   el: '#map', lat: 23.790223, lng: 90.414036, zoom: 13, zoomControl : true, 
-    //   zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
-    // });
-    // add address markers
-    // map.addMarker({ lat: 23.790223, lng: 90.414036, title: 'BD InfoSys',
-    //   infoWindow: { content: '<p>Building # 2, Plot # 111, Road # 35, Gulshan - 2, Dhaka</p>' } });
-
-    /*----------------------------------------------------*/
-    /*  contact form
-    ------------------------------------------------------*/
-
-    // Get the form.
-    var form = $('#contactForm');
-
-    // Get the messages div.
-    var formMessages = $('#form-messages');
-
-    // Set up an event listener for the contact form.
-    $(form).submit(function(e) {
-        // Stop the browser from submitting the form.
-        e.preventDefault();
-
-        // Serialize the form data.
-        var formData = $(form).serialize();
-
-        // Submit the form using AJAX.
-        $.ajax({
-                type: 'POST',
-                url: $(form).attr('action'),
-                data: formData
-            })
-            .done(function(response) {
-                // Make sure that the formMessages div has the 'success' class.
-                $(formMessages).removeClass('error');
-                $(formMessages).addClass('success');
-
-                // Set the message text.
-                $(formMessages).text(response);
-
-                // Clear the form.
-                $('#name').val('');
-                $('#email').val('');
-                $('#subject').val('');
-                $('#message').val('');
-            })
-            .fail(function(data) {
-                // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
-
-                // Set the message text.
-                if (data.responseText !== '') {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                }
-            });
-
-    });
-
+$(function(){
+	'use strict';
+
+	/*========================================
+		SmoothScroll
+	==========================================*/
+	smoothScroll.init({
+		updateURL: false
+	});
+
+	/*========================================
+		Adjust Menu Button's Color
+	==========================================*/
+	var c_height = $('.main-section').outerHeight() - 40,
+		$body = $('body'),
+		$menu_btn = $('.menu-btn');
+	function menu_btn_color(){
+		if( $(window).scrollTop() >= c_height ){
+			$menu_btn.removeClass('white-btn');
+		}else{
+			$menu_btn.addClass('white-btn');
+		}
+	}
+	menu_btn_color();
+
+
+	$(window).on('resize', function(){
+
+		c_height = $('.main-section').outerHeight() - 40;
+
+		setTimeout(function(){
+			AOS.refreshHard();
+		}, 800);
+
+	}).on('load', function(){
+
+		$body.addClass('loaded');
+
+		/*========================================
+			Testimonials Slider
+		==========================================*/
+		$('.testimonials-slider').owlCarousel({
+			items: 1
+		});
+
+		/*========================================
+			Project Images Slider
+		==========================================*/
+		$('.project-slider').owlCarousel({
+			items: 1,
+			autoHeight: true
+		});
+
+		/*========================================
+			Portfolio Items
+		==========================================*/
+		$('.portfolio-items').shuffle();
+
+		/*========================================
+			AnimateOnScroll
+		==========================================*/
+		AOS.init({
+			offset: 40,
+			disable: 'mobile',
+			duration: 600
+		});
+
+
+	}).on('scroll', function(){
+
+		menu_btn_color();
+
+	});
+
+	/*========================================
+		Material Design Ripples
+	==========================================*/
+	Waves.attach('.btn-custom, .menu li > a', 'waves-classic');
+	Waves.init();
+
+	/*========================================
+		Material Design Textbox
+	==========================================*/
+	$('.material-input > .form-control').blur(function() {
+		if ($(this).val()){
+			$(this).addClass('used');
+		}else{
+			$(this).removeClass('used');
+		}
+	});
+
+	/*========================================
+		Portfolio Items Ripple Effect
+	==========================================*/
+	$('.portfolio-items > li > .inner').each(function(){
+		var $this = $(this),
+			_w = $this.outerWidth(),
+			_h = $this.outerHeight(),
+			_s = _w > _h ? _w : _h,
+			_s = _s * 2.6;
+		$this.append('<div class="ripple" ></div>');
+		$this.find('.ripple').css({
+			height: _s,
+			width: _s
+		});
+	}).on('mouseenter', function(e){
+		var $this = $(this),
+			_s = parseInt($this.find('.ripple').css('height')),
+			_offset = $this.offset(),
+			_x = e.pageX - $this.offset().left,
+			_y = e.pageY - $this.offset().top,
+			_x = _x - (_s/2),
+			_y = _y - (_s/2);
+		$this.find('.ripple').css({
+			'top': _y,
+			'left': _x
+		});
+	});
+
+	/*========================================
+		Menu Functions
+	==========================================*/
+	$menu_btn.on('click', function(e){
+		e.preventDefault();
+		$body.toggleClass('show-menu');
+	});
+	$('.menu li > a').on('click', function(e){
+		$body.removeClass('show-menu');
+	});
+
+
+	/*=========================================================================
+		Contact Form (NOT WORKING IN DEMO ONLY)
+	=========================================================================*/
+	$('#contact-form').validator().on('submit', function (e) {
+		if (!e.isDefaultPrevented()) {
+			e.preventDefault();
+			var $this = $(this),
+				//You can edit alerts here
+				alerts = {
+					success:
+					"<div class='form-group' >\
+						<div class='alert alert-success' role='alert'> \
+							<strong>Message Sent!</strong> We'll be in touch as soon as possible\
+						</div>\
+					</div>",
+					error:
+					"<div class='form-group' >\
+						<div class='alert alert-danger' role='alert'> \
+							<strong>Oops!</strong> Sorry, an error occurred. Try again.\
+						</div>\
+					</div>"
+				};
+			$('#contact-form-result').html(alerts.success);
+			$('#contact-form').trigger('reset');
+			$('#contact-form .used').removeClass('used');
+		}
+	});
+
+
+	// Demo Code
+	if( typeof Storage != 'undefined' ){
+
+		var color = window.localStorage.getItem('color');
+
+		if(color != null ){
+			$body.addClass(color);
+		}
+
+	}
+	$('.color-switch > a').on('click', function(e){
+
+		e.preventDefault();
+
+		color = this.className.replace('c-', '');
+
+		if( typeof Storage != 'undefined' ){
+			window.localStorage.setItem('color', color);
+		}
+
+
+		$body.removeClass('pink green blue teal blue-grey default purple');
+
+		$body.addClass(color);
+
+	});
 
 });
