625316c45bf53369ec5c61a1caa1a1e3ee5f9c74
Michael La Plante <michael@michaellaplante.com>
2026-03-23 08:22:31 -0400

    feat: add @astrojs/rss and @astrojs/sitemap dependencies and sitemap integration

--- blog-src/astro.config.mjs
+++ blog-src/astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://michaellaplante.com',
   outDir: '../blog',
   base: '/blog',
   output: 'static',
   build: {
     format: 'directory',
   },
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       themes: {
